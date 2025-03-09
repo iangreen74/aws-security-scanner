34e0b56141767c66a3d19c34e5944042a7f80bc3
Ian Green <mikahiangreen@gmail.com>
2025-03-09 16:12:34 -0700

    fix variable

--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -1,3 +1,8 @@
 variable "notification_email" {
   default = "your-email@example.com"
 }
+
+variable "lambda_role_arn" {
+  description = "ARN of the IAM Role for Lambda execution"
+  type        = string
+}
