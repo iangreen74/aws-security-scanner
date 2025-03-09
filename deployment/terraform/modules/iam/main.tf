b438e58d3a061c505585d92392a4dadf00e996de
Ian Green <mikahiangreen@gmail.com>
2025-03-09 15:57:58 -0700

    Update main.tf

--- deployment/terraform/modules/iam/main.tf
+++ deployment/terraform/modules/iam/main.tf
@@ -55,9 +55,3 @@ resource "aws_iam_role_policy_attachment" "lambda_sns_attach" {
   role       = aws_iam_role.lambda_exec[0].name
   policy_arn = aws_iam_policy.lambda_sns_publish.arn
 }
-
-# OUTPUTS
-output "lambda_role_arn" {
-  description = "ARN of the Lambda Execution Role"
-  value       = length(aws_iam_role.lambda_exec) > 0 ? aws_iam_role.lambda_exec[0].arn : data.aws_iam_role.existing.arn
-}
