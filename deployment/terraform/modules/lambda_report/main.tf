cbd42fe10c19b62681de0b4b93ec968c356f65b2
Ian Green <mikahiangreen@gmail.com>
2025-03-09 14:20:53 -0700

    Update main.tf

--- deployment/terraform/modules/lambda_report/main.tf
+++ deployment/terraform/modules/lambda_report/main.tf
@@ -1,6 +1,12 @@
+resource "random_string" "lambda_suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 resource "aws_lambda_function" "lambda_report" {
   function_name = "ReportGenerator-${random_string.lambda_suffix.result}"
-  role          = var.lambda_role_arn # ✅ Use the IAM role passed as a variable
+  role          = var.lambda_role_arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.9"
 
