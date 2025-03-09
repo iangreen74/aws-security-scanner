985bae251e222985794ef675919c1e814de90f28
Ian Green <mikahiangreen@gmail.com>
2025-03-09 10:17:46 -0700

    fix deploy

--- deployment/terraform/modules/iam/main.tf
+++ deployment/terraform/modules/iam/main.tf
@@ -19,21 +19,31 @@ resource "aws_iam_role" "lambda_exec" {
   })
 }
 
+data "aws_iam_policy" "existing_lambda_sns_publish" {
+  name = "LambdaSNSPublishPolicy"
+}
+
 resource "aws_iam_policy" "lambda_sns_publish" {
+  count       = length(data.aws_iam_policy.existing_lambda_sns_publish) == 0 ? 1 : 0
   name        = "LambdaSNSPublishPolicy"
   description = "Allows Lambda to publish to SNS"
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "sns:Publish"
-        Resource = var.sns_topic_arn
-      }
-    ]
+    Statement = [{
+      Effect   = "Allow"
+      Action   = "sns:Publish"
+      Resource = "*"
+    }]
   })
 }
+
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+  upper   = false
+}
+
 
 resource "aws_iam_role_policy_attachment" "lambda_sns_attach" {
   role       = aws_iam_role.lambda_exec.name
