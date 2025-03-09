942e4108e2b9823c55fc47abc6049b8d8fdc1c21
Ian Green <mikahiangreen@gmail.com>
2025-03-09 15:37:54 -0700

    Update outputs.tf

--- deployment/terraform/modules/iam/outputs.tf
+++ deployment/terraform/modules/iam/outputs.tf
@@ -5,5 +5,5 @@ output "lambda_sns_policy_arn" {
 
 output "lambda_role_arn" {
   description = "ARN of the Lambda Execution Role"
-  value       = length(aws_iam_role.lambda_exec) > 0 ? aws_iam_role.lambda_exec[0].arn : null
+  value       = aws_iam_role.lambda_exec.arn
 }
